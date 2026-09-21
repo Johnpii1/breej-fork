14c30e370313bf12928b71239a4c400b185a23e0
Johnpii1 <okolichimobi08@gmail.com>
2026-09-21 10:43:09 +0100

    added our partners page

--- frontend/src/pages/marketing/Home.tsx
+++ frontend/src/pages/marketing/Home.tsx
@@ -1,11 +1,18 @@
-
-
-import Hero from "../../components/Hero"
+import Hero from "../../components/Hero";
+import OurPartners from "../../components/Our partners";
 
 export default function Home() {
-    return (
-        < >
-            <Hero />
-        </>
-    )
+  return (
+    <>
+      {/* HERO */}
+      <div>
+        <Hero />
+      </div>
+
+      {/* OUR PARTNERS */}
+      <div className="mb-4">
+        <OurPartners />
+      </div>
+    </>
+  );
 }
