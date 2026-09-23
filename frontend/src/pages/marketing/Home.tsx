65cc0a904c6b1692334ac8b05abe8848a4ee79ac
chiscookeke11 <chiscookeke11@gmail.com>
2026-09-23 07:54:43 +0100

    Fix

--- frontend/src/pages/marketing/Home.tsx
+++ frontend/src/pages/marketing/Home.tsx
@@ -4,22 +4,9 @@ import OurPartners from "../../components/Our partners";
 export default function Home() {
   return (
     <>
-      {/* HERO */}
-      <div>
-        <Hero />
-      </div>
-
-      {/* OUR PARTNERS */}
-      <div className="mb-4">
-        <OurPartners />
-      </div>
+      <Hero />
+      <OurPartners />
     </>
   );
-}
-    return (
-        <div className="bg-red-600 " >
-            Home page
-        </div>
-    )
 }
 
